80a3190d273efcf4068ba0353ce6d24b6a3a1fa9
harvey <heweimr@gmail.com>
2017-05-09 15:54:04 +0800

    mysql测试成功

--- src/test/MySQL.java
+++ src/test/MySQL.java
@@ -7,16 +7,14 @@ public static void main(String[] args){
 
            // 驱动程序名
            String driver = "com.mysql.jdbc.Driver";
-
            // URL指向要访问的数据库名scutcs
-           String url = "jdbc:mysql://127.0.0.1:3306/scutcs";
-
+           String url = "jdbc:mysql://127.0.0.1:3306/study";
+           url+="?autoReconnect=true&useSSL=false";
            // MySQL配置时的用户名
            String user = "root"; 
-
            // MySQL配置时的密码
            String password = "root";
-
+           
            try { 
             // 加载驱动程序
             Class.forName(driver);
@@ -29,32 +27,37 @@ public static void main(String[] args){
 
             // statement用来执行SQL语句
             Statement statement = conn.createStatement();
+            
+            //insert
+            String sqlInsert = "INSERT INTO Environment " +
+                    "VALUES (10, 20, 18)";
+            statement.executeUpdate(sqlInsert);
 
             // 要执行的SQL语句
-            String sql = "select * from student";
-
+            String sql = "select * from Environment";
             // 结果集
             ResultSet rs = statement.executeQuery(sql);
 
-            System.out.println("-----------------");
+            
+            System.out.println("--------------------");
             System.out.println("执行结果如下所示:");
-            System.out.println("-----------------");
-            System.out.println(" 学号" + "\t" + " 姓名");
-            System.out.println("-----------------");
+            System.out.println("--------------------");
+            System.out.println("id"+ "\t" + "学号" + "\t" + " 姓名");
+            System.out.println("--------------------");
 
             String name = null;
 
             while(rs.next()) {
     
              // 选择sname这列数据
-             name = rs.getString("sname");
+             name = rs.getString("humidity");
     
              // 首先使用ISO-8859-1字符集将name解码为字节序列并将结果存储新的字节数组中。
              // 然后使用GB2312字符集解码指定的字节数组
              name = new String(name.getBytes("ISO-8859-1"),"GB2312");
 
              // 输出结果
-             System.out.println(rs.getString("sno") + "\t" + name);
+             System.out.println( rs.getString("id") + "\t" + rs.getString("temperature") + "\t" + name );
             }
 
             rs.close();
